c7f235bd90f6d59db26f503637cbdab32c78b543
harsh-cedcoss <harshshukla@cedcoss.com>
2024-01-17 14:22:18 +0530

    user registration successful

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,6 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         //file has been uploaded successfully
         console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)    //removed the locally saved temporary file as the operation got failed
